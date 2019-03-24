35c3aad69721dd2ebc9b413a02ccafed51b7a6fc
testRasmi <test.rasmi@gmail.com>
2019-03-24 12:00:51 -0400

    two files

---
+++ InstanceVarTwo.java
@@ -0,0 +1,12 @@
+package com.gitYo;
+
+public class InstanceVarTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InstanceVariables obj= new InstanceVariables("Arif");
+		obj.setSalary(1000000);
+		obj.printEmpInfo();
+	}
+
+}
---
+++ MainforDog.java
@@ -0,0 +1,15 @@
+package com.gitYo;
+
+public class MainforDog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		dog obj = new dog("bob");
+		obj.setAge(8);
+		obj.getAge();
+		System.out.println("Variable Value :" +obj.dogAge);
+		
+
+	}
+
+}
